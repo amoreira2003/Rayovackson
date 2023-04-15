cdb5b73d461585984d1f38cbfc6d260607ed9301
Alexandre Moreira <alexrmoreira777@gmail.com>
2023-04-15 17:49:15 -0300

    Initial commit

---
+++ hovacker.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+
+plt.plot([0,10,20,30],[0,1,2,3]);
+fig, ax = plt.subplots()
+ax.set(xlabel="Teste",ylabel = "Teste 2", title="Coisas que o rayovac me pede :(");
+plt.show();
